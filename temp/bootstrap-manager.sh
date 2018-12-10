37bfe294edcd6b0b66b01681fb0fc7892fcc04fb
Pawandeep <pawandeep.thind@gmail.com>
2018-12-10 14:07:19 -0500

    Cleaned up the temp folder

--- temp/bootstrap-manager.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-su vagrant <<'EOF'
-cp server/keys/private ~/.ssh/id_rsa
-cp server/keys/public ~/.ssh/id_rsa.pub
-cp server/keys/public ~/.ssh/authorized_keys
-chmod 0600 ~/.ssh/*
-EOF
